cc3a57036a2564c7bb338c3cc28b1ab6c93861a6
Vrund <vrundraval24@gmail.com>
2024-01-16 02:35:30 +0530

    cloudinary and multer.

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+const cloudinaryFileUpload = async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null;
+
+        // try upload file to cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        // if file upload is successful
+        console.log('File is successfully uploaded to cloudinary.', response.url)
+
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // remove the locally saved temporary file if the upload is failed
+        return null;
+    }
+}
+
+export {cloudinaryFileUpload};
